13113a99347d32c5734a65a4b2d65aedbb3230ff
巨辰妍 <1056710249@qq.com>
2016-10-15 22:17:37 +0800

    修改底部菜单

--- app/src/main/java/com/example/angela/fulicenter/activity/MainActivity.java
+++ app/src/main/java/com/example/angela/fulicenter/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.example.angela.fulicenter.R;
 import com.example.angela.fulicenter.utlils.L;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +12,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-///7        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
         L.d("MainActivity onCreate");
     }
-    public  void onClick(View view){
-
+    public void onnewgoods(View view) {
     }
+
+    public void onPersonal(View view) {
+    }
+
+    public void oncategory(View view) {
+    }
+
+    public void onboutique(View view) {
+    }
+
+    public void oncart(View view) {
+    }
+    
 }
