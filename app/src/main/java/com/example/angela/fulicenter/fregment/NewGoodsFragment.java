d0f296bcaaf6c58281679a3b9c6bdf8f55404c85
巨辰妍 <1056710249@qq.com>
2016-10-18 00:03:10 +0800

    修改newgoodsfragment类

---
+++ app/src/main/java/com/example/angela/fulicenter/fregment/NewGoodsFragment.java
@@ -0,0 +1,91 @@
+package com.example.angela.fulicenter.fregment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.angela.fulicenter.I;
+import com.example.angela.fulicenter.R;
+import com.example.angela.fulicenter.activity.MainActivity;
+import com.example.angela.fulicenter.adapter.GoodsAdapter;
+import com.example.angela.fulicenter.bean.NewGoodsBean;
+import com.example.angela.fulicenter.net.NetDao;
+import com.example.angela.fulicenter.net.OkHttpUtils;
+import com.example.angela.fulicenter.utlis.ConvertUtils;
+import com.example.angela.fulicenter.utlis.L;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+
+/**
+ * Created by xiaomiao on 2016/10/17.
+ */
+
+public class NewGoodsFragment extends Fragment {
+
+    @BindView(R.id.tv_refresh)
+    TextView mtvRfresh;
+    @BindView(R.id.rv)
+    RecyclerView mrv;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout msrl;
+
+    MainActivity mContext;
+    GoodsAdapter mAdapter;
+    ArrayList<NewGoodsBean> mList;
+    int pageId=1;
+
+
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_newgoods, container, false);
+        ButterKnife.bind(this, layout);
+        mContext = (MainActivity) getContext();
+        mList = new ArrayList<>();
+        mAdapter = new GoodsAdapter(mContext,mList);
+        initView();
+        initData();
+        return layout;
+    }
+
+    private void initData() {
+        NetDao.downloadNewGoods(mContext, pageId, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
+            @Override
+            public void onSuccess(NewGoodsBean[] result) {
+                     L.e("sdfafdafa");
+                if (result != null && result.length > 0) {
+                    ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
+                    mAdapter.initData(list);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                L.e("error: "+error);
+            }
+        });
+    }
+
+    private void initView() {
+        msrl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        GridLayoutManager glm = new GridLayoutManager(mContext, I.COLUM_NUM);
+        mrv.setLayoutManager(glm);
+        mrv.setHasFixedSize(true);
+        mrv.setAdapter(mAdapter);
+    }
+
+}
