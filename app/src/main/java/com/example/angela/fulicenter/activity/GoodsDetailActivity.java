0b703212cb7699f0624f7fb8670944568989a87d
巨辰妍 <1056710249@qq.com>
2016-10-18 22:47:45 +0800

    添加两个自定义控件

--- app/src/main/java/com/example/angela/fulicenter/activity/GoodsDetailActivity.java
+++ app/src/main/java/com/example/angela/fulicenter/activity/GoodsDetailActivity.java
@@ -2,19 +2,94 @@ package com.example.angela.fulicenter.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.angela.fulicenter.I;
 import com.example.angela.fulicenter.R;
+import com.example.angela.fulicenter.bean.GoodsDetailsBean;
+import com.example.angela.fulicenter.net.NetDao;
+import com.example.angela.fulicenter.net.OkHttpUtils;
+import com.example.angela.fulicenter.utlis.CommonUtils;
 import com.example.angela.fulicenter.utlis.L;
+import com.example.angela.fulicenter.view.FlowIndicator;
+import com.example.angela.fulicenter.view.SlideAutoLoopView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class GoodsDetailActivity extends AppCompatActivity {
 
+    @BindView(R.id.backClickArea)
+    LinearLayout mBackClickArea;
+    @BindView(R.id.tv_good_name_english)
+    TextView mTvGoodNameEnglish;
+    @BindView(R.id.tv_good_name)
+    TextView mTvGoodName;
+    @BindView(R.id.tv_good_price_shop)
+    TextView mTvGoodPriceShop;
+    @BindView(R.id.tv_good_price_current)
+    TextView mTvGoodPriceCurrent;
+    @BindView(R.id.salv)
+    SlideAutoLoopView mSalv;
+    @BindView(R.id.indicator)
+    FlowIndicator mIndicator;
+    @BindView(R.id.wv_good_brief)
+    WebView mWvGoodBrief;
+    int goodsId;
+
+    GoodsDetailActivity mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
-        int goodsID = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
-        L.d("details","goodsid="+goodsID);
+        ButterKnife.bind(this);
+        goodsId= getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
+        L.d("details", "goodsid=" + goodsId);
+        if (goodsId==0){
+            finish();
+        }
+        mContext=this;
+        initView();
+        initData();
+        setListener();
+    }
+    private void setListener() {
 
+    }
+    private void initData() {
+        NetDao.downloadGoodsDetail(mContext, goodsId, new OkHttpUtils.OnCompleteListener<GoodsDetailsBean>() {
+            @Override
+            public void onSuccess(GoodsDetailsBean result) {
+                L.d("details="+result);
+                if (result!=null){
+                    showGoodDetails(result);
+                }else {
+                    finish();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                finish();
+                L.d("details,error="+error);
+                CommonUtils.showShortToast(error);
+
+            }
+        });
+
+    }
+
+    private void showGoodDetails(GoodsDetailsBean details) {
+        mTvGoodNameEnglish.setText(details.getGoodsEnglishName());
+        mTvGoodName.setText(details.getGoodsName());
+        mTvGoodPriceCurrent.setText(details.getCurrencyPrice());
+        mTvGoodPriceShop.setText(details.getShopPrice());
+
+
+    }
+
+    private void initView() {
     }
 }
